dbc3665955617aaa7b78d5977372c519b6292f8b
Yurii Myronchuk <bladeroot@gmail.com>
2021-11-03 10:15:35 +0200

    Add foreign NS servers guide (#8)

--- src/faq_source/01-dns/03-errors/01-the-object.php
+++ src/faq_source/01-dns/03-errors/01-the-object.php
@@ -10,5 +10,8 @@ $this->title = 'The "Object does not exist" error during NS-server installation'
 <p>
     To create subsidiary servers, please read the <a href="#03-creating">"Creating a subsidiary NS-server" guide</a>.
 </p>
+<p>
+    Seeing this error when using the name servers, another company provided to you? Then, please, contact that company support team and ask to create these NSs in their domain control panel.
+</p>
 
 
